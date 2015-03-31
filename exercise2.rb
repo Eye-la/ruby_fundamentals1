f18dae67efab08af9aac31de03e0d64df5f724ca
Aila <zee2198@gmail.com>
2015-03-31 15:44:11 -0400

    Exercise 1&2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+#Ruby Fundamentals: Part 1: Exercise 2
+
+#1. 
+
+tip = 55 * 0.13
+puts tip
+
+#2. 
+
+puts "Exercise " + 2.to_s
+
+#3.
+
+add_3 = 45628 * 7839
+puts "A large number is #{add_3}"
+
+#4.
+
+puts (true && false)||(false && true)||!(false && false)
+#ans.true	
